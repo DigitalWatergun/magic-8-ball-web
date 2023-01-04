ba905e88a9e7bb52203a4ed5309d7ae52af45c27
Philip <philipwinduong@gmail.com>
2023-01-03 22:12:15 -0800

    Create Vue app, add magic 8 ball component & images

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <Magic8Ball />
+</template>
+
+<script>
+// import HelloWorld from "./components/HelloWorld.vue";
+import Magic8Ball from "./components/Magic8Ball.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HelloWorld,
+    Magic8Ball,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Magic8Ball.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="magic8ball">
+    <h1>Magic 8 Ball</h1>
+    <img class="ball-image" src="./../assets/magic-8-ball.png" />
+    <button class="startButton">Start</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Magic8Ball",
+  data() {
+    return {
+      shake: false,
+      defaultImg: "./../assets/magic-8-ball.png",
+      srcs: [
+        "./../assets/magic-8-ball.png",
+        "./../assets/magic-8-ball-tri.png",
+      ],
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style>
+.ball-image {
+  width: 400px;
+  height: 400px;
+}
+
+.magic8ball {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+.startButton {
+  display: block;
+}
+</style>
