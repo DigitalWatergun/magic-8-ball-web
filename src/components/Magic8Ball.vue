254658d94ad7dc364b486f655197eb5a3d619726
Philip <philipwinduong@gmail.com>
2023-01-04 21:57:22 -0800

    Add responses and disable text/button when pressed

--- src/components/Magic8Ball.vue
+++ src/components/Magic8Ball.vue
@@ -1,25 +1,57 @@
 <template>
   <div class="magic8ball">
     <h1>Magic 8 Ball</h1>
-    <img class="ball-image" src="./../assets/magic-8-ball.png" />
-    <button class="startButton">Start</button>
+    <img
+      class="ball-image"
+      src="./../assets/magic-8-ball.png"
+      v-on:click="changePicture()"
+    />
+    <p>{{ response }}</p>
+    <input
+      id="questionbox"
+      type="text"
+      class="questionbox"
+      placeholder="Ask a question..."
+    />
+    <button class="startbutton" @click="start" :disabled="isShaking">
+      {{ buttonText }}
+    </button>
   </div>
 </template>
 
 <script>
+import generateResponse from "../responses/generateResponse";
+
 export default {
   name: "Magic8Ball",
   data() {
     return {
-      shake: false,
-      defaultImg: "./../assets/magic-8-ball.png",
-      srcs: [
+      defaultImg: "/src/assets/magic-8-ball.png",
+      images: [
         "./../assets/magic-8-ball.png",
         "./../assets/magic-8-ball-tri.png",
       ],
+      response: "",
+      buttonText: "Shake 8 Ball",
+      isShaking: false,
+      index: 0,
     };
   },
-  methods: {},
+  methods: {
+    start() {
+      this.isShaking = true;
+      document.getElementById("questionbox").disabled = true;
+      setTimeout(() => {
+        this.isShaking = false;
+        document.getElementById("questionbox").disabled = false;
+        this.response = generateResponse();
+        this.buttonText = "Shake again!";
+      }, 3000);
+    },
+    changeImage() {
+      this.index = ++this.index % this.images.length;
+    },
+  },
 };
 </script>
 
@@ -36,7 +68,14 @@ export default {
   align-items: center;
 }
 
-.startButton {
+.questionbox {
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  width: 200px;
+}
+
+.startbutton {
   display: block;
 }
 </style>
